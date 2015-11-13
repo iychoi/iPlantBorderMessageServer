9f52d3c58454b6d5fe9ce1d0a55dfcc28e4d2440
Illyoung Choi <iychoi@email.arizona.edu>
2015-11-12 22:53:19 -0700

    Add startup script

---
+++ start.sh
@@ -0,0 +1,5 @@
+echo "*************************"
+echo "iPlantBorderMessageServer"
+echo "*************************"
+java -cp libs/*:dist/iPlantBorderMessageServer.jar org.iplantcollaborative.IPlantBorderMessageServer $@
+
